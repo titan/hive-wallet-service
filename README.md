3d5bd9d88ab54aa182514208415c69b2f90395a1
Titan <howay.tan@gmail.com>
2016-08-11 17:06:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plan Service
+
+## typings
+```shell
+typings install --save --global dt~node
+typings install --save --global dt~redis
+typings install --global msgpack-lite=http://git.fengchaohuzhu.com:10080/Titan/typescript.d.ts/raw/master/msgpack-lite.d.ts
+typings install --global nanomsg=http://git.fengchaohuzhu.com:10080/Titan/typescript.d.ts/raw/master/nanomsg-lite.d.ts
+```
---
+++ src/cli.ts
@@ -0,0 +1,26 @@
+import * as nano from 'nanomsg';
+import * as msgpack from 'msgpack-lite';
+
+interface Context {
+  domain: string,
+  ip: string,
+  uid: string
+}
+
+let req = nano.socket('req');
+
+let addr = 'tcp://0.0.0.0:4040';
+
+req.connect(addr);
+
+let params = {
+  ctx: {domain: 'mobile', ip: 'localhost', uid: ''},
+  fun: 'getAvailablePlans',
+  args: []
+};
+
+req.send(msgpack.encode(params));
+req.on('data', function (msg) {
+  console.log(msgpack.decode(msg));
+  req.shutdown(addr);
+});
