25e3fedeadae44a9349877fa39cab51d88a01223
Titan <howay.tan@gmail.com>
2016-08-15 11:28:52 +0800

    Adapte new hive-service module

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { Service, Config, Context, CallbackFunction, Permission } from 'hive-service';
+import { Service, Config, Context, ResponseFunction, Permission } from 'hive-service';
 import * as Redis from "redis";
 
 let redis = Redis.createClient();
@@ -17,46 +17,46 @@ let svc = new Service(config);
 
 let permissions: Permission[] = [['mobile', true], ['admin', true]];
 
-svc.call('getAvailablePlans', permissions, (ctx: Context, cb: CallbackFunction) => {
+svc.call('getAvailablePlans', permissions, (ctx: Context, rep: ResponseFunction) => {
   // http://redis.io/commands/sdiff
   redis.sdiff(list_key, entities_prefix + ctx.uid, function (err, result) {
     if (err) {
-      cb([]);
+      rep([]);
     } else {
-      ids2objects(entity_key, result, cb);
+      ids2objects(entity_key, result, rep);
     }
   });
 });
 
-svc.call('getJoinedPlans', permissions, (ctx: Context, cb: CallbackFunction) => {
+svc.call('getJoinedPlans', permissions, (ctx: Context, rep: ResponseFunction) => {
   // http://redis.io/commands/smembers
   redis.smembers(entities_prefix + ctx.uid, function (err, result) {
     if (err) {
-      cb([]);
+      rep([]);
     } else {
-      ids2objects(entity_key, result, cb);
+      ids2objects(entity_key, result, rep);
     }
   });
 });
 
-svc.call('getPlanItems', permissions, (ctx: Context, cb: CallbackFunction, pid: string) => {
+svc.call('getPlanItems', permissions, (ctx: Context, rep: ResponseFunction, pid: string) => {
   // http://redis.io/commands/lrange
   redis.lrange(items_prefix + pid, 0, -1, function (err, result) {
     if (err) {
-      cb([]);
+      rep([]);
     } else {
-      ids2objects(item_key, result, cb);
+      ids2objects(item_key, result, rep);
     }
   });
 });
 
-function ids2objects(key: string, ids: string[], cb: CallbackFunction) {
+function ids2objects(key: string, ids: string[], rep: ResponseFunction) {
   let multi = redis.multi();
   for (let id in ids) {
     multi.hget(key, id);
   }
   multi.exec(function(err, replies) {
-    cb(replies);
+    rep(replies);
   });
 }
 
