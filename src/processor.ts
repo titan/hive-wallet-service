afa9470968e6ac8524ea8ae5b95a3b8aaafa0f8e
Titan <howay.tan@gmail.com>
2016-08-20 12:04:03 +0800

    Use hive processor framework

--- src/processor.ts
+++ src/processor.ts
@@ -1,78 +1,59 @@
-import * as nanomsg from 'nanomsg';
-import * as Pool from 'pg-pool';
-import * as pg from 'pg';
-import * as msgpack from 'msgpack-lite';
-import * as Redis from 'redis';
+import { Processor, Config, ModuleFunction, DoneFunction } from 'hive-processor';
+import { Client as PGClient, ResultSet } from 'pg';
+import { createClient, RedisClient} from 'redis';
 
-let addr = "ipc:///tmp/queue.ipc";
-let pull = nanomsg.socket('pull');
-pull.connect(addr);
-
-var config = {
-  host: process.env['DB_HOST'],
-  user: process.env['DB_USER'],
+let config: Config = {
+  dbhost: process.env['DB_HOST'],
+  dbuser: process.env['DB_USER'],
   database: process.env['DB_NAME'],
-  password: process.env['DB_PASSWORD'],
-  port: 5432,
-  min: 1, // min number of clients in the pool
-  max: 2, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  dbpasswd: process.env['DB_PASSWORD'],
+  cachehost: process.env['CACHE_HOST'],
+  addr: "ipc:///tmp/queue.ipc"
 };
 
-let pool = new Pool(config);
+let processor = new Processor(config);
 
-pull.on('data', (input: any) => {
-  let buf = msgpack.decode(input);
-  if (buf.cmd == 'refresh') {
-    pool.connect().then(client => {
-      client.query('SELECT id, title, description, image, thumbnail, period FROM plans', [], (err: Error, result: pg.ResultSet) => {
-        if (err) {
-          console.error('query error', err.message, err.stack);
-          return;
+processor.call('refresh', (db: PGClient, cache: RedisClient, done: DoneFunction) => {
+  db.query('SELECT id, title, description, image, thumbnail, period FROM plans', [], (err: Error, result: ResultSet) => {
+    if (err) {
+      console.error('query error', err.message, err.stack);
+      return;
+    }
+    let plans = [];
+    for (let row of result.rows) {
+      plans.push(row2plan(row));
+    }
+    let countdown = plans.length; // indicate how many async callings are running
+    for (let plan of plans) {
+      db.query('SELECT id, name, title, description FROM plan_rules WHERE pid = $1', [ plan.id ], (err1: Error, result1: ResultSet) => {
+        countdown -= 1;
+        if (err1) {
+          console.error('query error', err1.message, err1.stack);
+        } else {
+          for (let row of result1.rows) {
+            plan.rules.push(row2rule(row));
+          }
         }
-        let plans = [];
-        for (let row of result.rows) {
-          plans.push(row2plan(row));
-        }
-        let countdown = plans.length; // indicate how many async callings are running
-        for (let plan of plans) {
-          client.query('SELECT id, name, title, description FROM plan_rules WHERE pid = $1', [ plan.id ], (err1: Error, result1: pg.ResultSet) => {
-            countdown -= 1; 
-            if (err1) {
-              console.error('query error', err1.message, err1.stack);
-            } else {
-              for (let row of result1.rows) {
-                plan.rules.push(row2rule(row));
-              }
+        if (countdown == 0) {
+          // all query are done
+          let multi = cache.multi();
+          for (let plan of plans) {
+            multi.hset("plan-entities", plan.id, JSON.stringify(plan));
+          }
+          for (let plan of plans) {
+            multi.sadd("plans", plan.id);
+          }
+          multi.exec((err, replies) => {
+            if (err) {
+              console.error(err);
             }
-            if (countdown == 0) {
-              // all query are done
-              client.end();
-              let redis = Redis.createClient(6379, process.env['CACHE_HOST']); // port, host
-              let multi = redis.multi();
-              for (let plan of plans) {
-                multi.hset("plan-entities", plan.id, JSON.stringify(plan));
-              }
-              for (let plan of plans) {
-                multi.sadd("plans", plan.id);
-              }
-              multi.exec((err, replies) => {
-                if (err) {
-                  console.error(err);
-                }
-                redis.quit();
-              });
-            }
+            done(); // close db and cache connection
           });
         }
       });
-    });
-  }
+    }
+  });
 });
-
-pool.on('error', function (err, client) {
-  console.error('idle client error', err.message, err.stack)
-})
 
 function row2plan(row) {
   return {
@@ -95,4 +76,6 @@ function row2rule(row) {
   };
 }
 
-console.log('Start processor at ' + addr);
+processor.run();
+
+console.log('Start processor at ' + config.addr);
